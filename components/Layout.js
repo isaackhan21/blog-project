23f1c4fe7626d3b6595b1d45b5b3877db17089c7
Awesomecoder26 <isaackhan21@gmail.com>
2021-11-29 16:17:12 +0000

    Added styling to the titles

--- components/Layout.js
+++ components/Layout.js
@@ -2,19 +2,23 @@ import Link from "next/link";
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <div className="container flex justify-center mx-auto">
       <header>
         <Link href="/">
           <a>
-            <h1>Web Dev Fanatic</h1>
-            <h3>Me Personal Web Dev Blog</h3>
+            <div className="ml-12 mt-8">
+              <h1 className="text-7xl  font-bold">Web Dev Fanatic</h1>
+              <h2 className="text-4xl mt-4 font-semibold">
+                My Personal Web Dev Blog
+              </h2>
+            </div>
           </a>
         </Link>
       </header>
 
       <div className="">{children}</div>
       <footer>
-        <p>Copyright 2021 Web Dev Fantic</p>
+        <p>Copyright 2021 Web Dev Fanatic</p>
       </footer>
     </div>
   );
