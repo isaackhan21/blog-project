b7ab81bddf256a32bc2a4c5fc0d7afb35ec4f985
Awesomecoder26 <isaackhan21@gmail.com>
2021-12-01 09:46:24 +0000

    Minor styling to NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,17 +1,17 @@
 export default function NavBar() {
   return (
-    <nav className="mt-8 pt-2  border-t-2 ">
-      <ul className=" flex mx-auto px-60 text-gray-600  text-xl">
-        <li className="mx-6">
+    <nav className="mt-8 pt-2 border-t-2 ">
+      <ul className=" flex mx-auto mt-2 px-20 text-gray-500  text-xl">
+        <li className="mx-8">
           <a href="/">Home</a>
         </li>
-        <li className="mx-6">
+        <li className="mx-8">
           <a href="/about">About</a>
         </li>
-        <li className="mx-6">
+        <li className="mx-8">
           <a href="/contact">Contact</a>
         </li>
-        <li className="mx-6">
+        <li className="mx-8">
           <a href="/contact">Portfolio</a>
         </li>
       </ul>
