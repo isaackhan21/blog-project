bbb69c4afea694d74a566b943e938fbd2b7d3eb9
Awesomecoder26 <isaackhan21@gmail.com>
2021-12-01 17:46:01 +0000

    Added images to the project

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function PostCard({ post }) {
   const { title, slug, datePublished, thumbnail } = post.fields;
   return (
     <div>
-      <div>{}</div>
+      <div>
+        <Image
+          src={"https:" + thumbnail.fields.file.url}
+          width={thumbnail.fields.file.details.image.width}
+          height={thumbnail.fields.file.details.image.height}
+        />
+      </div>
       <div>
         <div>
           <h4>{title}</h4>
