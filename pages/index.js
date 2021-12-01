5a1e18b639459c905d7c24b5d7984ea1d65bbcbb
Awesomecoder26 <isaackhan21@gmail.com>
2021-12-01 13:39:12 +0000

    Created contentful client

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
 import Head from "next/head";
+import { createClient } from "contentful";
 
-export default function Home() {
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries({ content_type: "blogPost" });
+
+  return {
+    props: {
+      blogPosts: res.items,
+    },
+  };
+}
+
+export default function Home({ blogPosts }) {
+  console.log(blogPosts);
   return <div className="">Blog Post List</div>;
 }
