b1799ef74f601b3913bfbf6694b3911a2e1ad29e
Awesomecoder26 <isaackhan21@gmail.com>
2021-12-13 16:07:01 +0000

    Styled Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center">
+    <footer className=" flex justify-center items-center border-t-2 border-gray-200 py-16 ">
       <p>Copyright 2021 Web Dev Fanatic</p>
     </footer>
   );
